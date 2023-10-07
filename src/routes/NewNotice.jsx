2336704826b63b38d535a0f4692b389845ef2992
Anton Krupin <krupin_anton@mail.ru>
2023-10-07 17:40:23 +0300

    add newNotice adding to DB

--- src/routes/NewNotice.jsx
+++ src/routes/NewNotice.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NewNotice = () => {
+  const textareaRef = useRef();
+  const formRef = useRef();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    textareaRef.current.focus();
+  }, []);
+
+  const addNewNotice = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current);
+    const newNotice = {};
+    newNotice.body = formData.get('noticeBody');
+    newNotice.creationDate = new Date().toLocaleString("ru");
+    if (newNotice.body) {
+      await fetch('http://localhost:5000/api/addNotice', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(newNotice)
+      });
+      navigate('/');
+    }
+  };
+
   return (
-    <h1>New notice</h1>
+    <div className="d-flex flex-column">
+      <h1>Новая заметка</h1>
+      <form onSubmit={addNewNotice} ref={formRef}>
+        <textarea
+          name="noticeBody"
+          placeholder="Текст заметки"
+          rows="4"
+          cols="50"
+          ref={textareaRef}
+        >
+
+        </textarea>
+        <button type="submit" className="btn btn-primary">Добавить</button>
+      </form>
+    </div>
   )
 };
 
