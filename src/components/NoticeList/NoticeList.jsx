497841ef91ca97d5b5901638eb7fd0754e591229
Anton Krupin <krupin_anton@mail.ru>
2023-10-07 18:01:24 +0300

    add getting notices

--- src/components/NoticeList/NoticeList.jsx
+++ src/components/NoticeList/NoticeList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Notice from '../Notice/Notice';
 
 import '../NoticeList/NoticeList.css';
@@ -22,14 +22,34 @@ const notices = [
 ]
 
 const NoticeList = () => {
+  const [notices, setNotices] = useState([]);
+  useEffect(() => {
+    const sendRequest = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/');
+        const responseData = await response.json();
+        setNotices(responseData.reverse());
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    sendRequest();
+  }, [notices]);
+
   return (
     <div className="notice_list">
       <h3>Список заметок:</h3>
       {notices.map((notice) => (
-        <Notice key={notice.id} id={notice.id} body={notice.body} creation_date={notice.creation_date}/>
+        <Notice key={notice._id} id={notice._id} body={notice.body} creation_date={notice.creationDate}/>
       ))}
     </div>
   )
 };
 
 export default NoticeList
+
+/*
+{notices.map((notice) => (
+        <Notice key={notice.id} id={notice.id} body={notice.body} creation_date={notice.creation_date}/>
+      ))}
+*/
