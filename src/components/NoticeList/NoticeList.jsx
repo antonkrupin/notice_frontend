6c248131393a2b6d23f8b88f129e175436b7e81c
Anton Krupin <krupin_anton@mail.ru>
2023-10-07 18:31:18 +0300

    delete notices array

--- src/components/NoticeList/NoticeList.jsx
+++ src/components/NoticeList/NoticeList.jsx
@@ -3,24 +3,6 @@ import Notice from '../Notice/Notice';
 
 import '../NoticeList/NoticeList.css';
 
-const notices = [
-  {
-    id: 1,
-    body: "first notice",
-    creation_date: '11.01.2023',
-  },
-  {
-    id: 2,
-    body: "second notice",
-    creation_date: '12.01.2023',
-  },
-  {
-    id: 3,
-    body: "third notice",
-    creation_date: '13.01.2023',
-  },
-]
-
 const NoticeList = () => {
   const [notices, setNotices] = useState([]);
   useEffect(() => {
