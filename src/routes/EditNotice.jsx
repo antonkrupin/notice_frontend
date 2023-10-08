347e4a1803ac5e03ab1fd238f94b07897f7c1804
Anton Krupin <krupin_anton@mail.ru>
2023-10-08 18:00:54 +0300

    add updateNotice function

--- src/routes/EditNotice.jsx
+++ src/routes/EditNotice.jsx
@@ -1,44 +1,84 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useRef } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
-const notices = [
-  {
-    id: 1,
-    body: "first notice",
-    creation_date: '11.01.2023',
-  },
-  {
-    id: 2,
-    body: "second notice",
-    creation_date: '12.01.2023',
-  },
-  {
-    id: 3,
-    body: "third notice",
-    creation_date: '13.01.2023',
-  },
-]
 
 const EditNotice = () => {
   const id = useParams().id;
-  const [notice] = notices.filter((notice) => parseInt(id) === notice.id);
+  const formRef = useRef();
+  const textareaRef = useRef();
+  const navigate = useNavigate();
+  const [notice, setNotice] = useState({});
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/api/editNotice/${id}`);
+        const responseData = await response.json();
+        setNotice(responseData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    sendRequest();
+    textareaRef.current.focus();
+  }, [notice, id]);
+
+  const updateNotice = async (e) => {
+    e.preventDefault();
+    const updatedNotice = {};
+    const formData = new FormData(formRef.current);
+
+    updatedNotice.body = formData.get('noticeBody');
+    if (updatedNotice.body) {
+      await fetch(
+        `http://localhost:5000/api/editNotice/${id}`,
+        {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(updatedNotice)
+        });
+        navigate('/');
+    }
+  }
 
   return (
-    <div className="notice d-flex justify-content-between" id={id}>
+    <div className="d-flex justify-content-center" id={id}>
       <div className="notice_creation_date">
-        <h4>Дата создания: {notice.creation_date}</h4>
+        <h5>Дата создания: {notice.creationDate}</h5>
       </div>
       <div className="notice_body">
-        <h3>
-          {notice.body}
-        </h3>
-      </div>
-      <div className="notice_buttons">
-        <button className="btn btn-success" >Сохранить</button>
-        <button className="btn btn-danger" >Удалить</button>
+        <form onSubmit={updateNotice} ref={formRef}>
+          <textarea
+            name="noticeBody"
+            defaultValue={notice.body}
+            ref={textareaRef}
+            rows="4"
+            cols="50"
+          >
+          </textarea>
+          <div className="notice_buttons">
+            <button
+              type="submit"
+              className="btn btn-success"
+            >
+              Сохранить
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   )
 };
 
 export default EditNotice;
+
+/*
+<button
+              className="btn btn-danger"
+              onClick={deleteNotice}
+            >
+              Удалить
+            </button>
+*/
