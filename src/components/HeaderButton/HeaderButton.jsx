a1d3e2cde387a98e18fa5ee5b0cde5a1dd7ea26e
antonkrupin <krupin_anton@mail.ru>
2023-10-09 15:16:53 +0300

    change

--- src/components/HeaderButton/HeaderButton.jsx
+++ src/components/HeaderButton/HeaderButton.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable default-case */
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
@@ -21,21 +20,39 @@ const HeaderButton = () => {
     case 'addNotice': {
       return (
         <div className="new_notice">
-          <button type="button" className="btn btn-primary" onClick={backToNoticeList}>{i18n.t('buttons.backBtn')}</button>
+          <button
+						type="button"
+						className="btn btn-primary"
+						onClick={backToNoticeList}
+					>
+						{i18n.t('buttons.backBtn')}
+					</button>
         </div>
       )
     }
     case 'editNotice': {
       return (
         <div className="new_notice">
-          <button type="button" className="btn btn-primary" onClick={backToNoticeList}>{i18n.t('buttons.backBtn')}</button>
+          <button
+						type="button"
+						className="btn btn-primary"
+						onClick={backToNoticeList}
+					>
+						{i18n.t('buttons.backBtn')}
+					</button>
         </div>
       )
     }
     default: {
       return (
         <div className="new_notice">
-          <button type="button" className="btn btn-primary" onClick={createNewNotice}>{i18n.t('buttons.newNoticeBtn')}</button>
+          <button
+						type="button"
+						className="btn btn-primary"
+						onClick={createNewNotice}
+					>
+						{i18n.t('buttons.newNoticeBtn')}
+					</button>
         </div>
       )
     }
--- src/components/Notice/Notice.jsx
+++ src/components/Notice/Notice.jsx
@@ -1,46 +1,74 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import i18n from '../../asserts/i18';
+import { deleteNotice, setStatus } from '../../slices/mainReducer';
+import { fetchStatus } from '../../slices/selectors';
 
 import '../Notice/Notice.css';
 
 const Notice = (props) => {
   const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const status = useSelector(fetchStatus);
   const { id, body, creation_date, editDate } = props;
 
-  const editNotice = () => {
+  const editNoticeHandler = () => {
     navigate(`/editNotice/${id}`);
   };
 
-  const deleteNotice = async () => {
+  const deleteNoticeHandler = async () => {
     try {
+			dispatch(setStatus('deleteNotice'));
       await fetch(`http://localhost:5000/api/notice/${id}`, {
         method: 'DELETE',
         headers: {},
         body: null
-      })
+      });
+			dispatch(setStatus(''));
     } catch (err) {
       console.log(err);
     }
+		dispatch(deleteNotice({id}));
   }
 
   return (
-    <div className="notice d-flex justify-content-between" id={id}>
-      <div className="notice_creation_date">
-        <h5 className="text-primary">{i18n.t('notice.creationDate')} {creation_date}</h5>
-        {editDate && (<h5 className="text-success">{i18n.t('notice.editDate')} {editDate}</h5>)}
-      </div>
-      <div className="notice_body">
-        <h3>
-          {body}
-        </h3>
-      </div>
-      <div className="notice_buttons">
-        <button className="btn btn-success" onClick={editNotice}>{i18n.t('buttons.editBtn')}</button>
-        <button className="btn btn-danger" onClick={deleteNotice}>{i18n.t('buttons.deleteBtn')}</button>
-      </div>
-    </div>
+    <>
+			{!status && (
+				<div className="notice d-flex justify-content-between" id={id}>
+					<div className="notice_creation_date">
+						<h5 className="text-primary">
+							{i18n.t('notice.creationDate')} {creation_date}
+						</h5>
+						{editDate && (
+							<h5 className="text-success">
+								{i18n.t('notice.editDate')} {editDate}
+							</h5>
+						)}
+					</div>
+					<div className="notice_body">
+						<h3>
+							{body}
+						</h3>
+					</div>
+					<div className="notice_buttons">
+						<button
+							className="btn btn-success"
+							onClick={editNoticeHandler}
+						>
+							{i18n.t('buttons.editBtn')}
+						</button>
+						<button
+							className="btn btn-danger"
+							onClick={deleteNoticeHandler}
+						>
+							{i18n.t('buttons.deleteBtn')}
+						</button>
+					</div>
+				</div>
+			)}
+		</>
   )
 };
 
