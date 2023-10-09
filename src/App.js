58901ddffa117f4991d385b6075f6e36425c551c
antonkrupin <krupin_anton@mail.ru>
2023-10-09 10:47:13 +0300

    import routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from './components/Header/Header';
 import NoticeList from './components/NoticeList/NoticeList';
 import NewNotice from './routes/NewNotice';
 import EditNotice from './routes/EditNotice';
+import routes from './routes/routes';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -20,9 +21,9 @@ const App = () => {
       <div className="main d-flex align-items-center flex-column">
         <Header />
         <Routes>
-          <Route path="/" element={<NoticeList />}/>
-          <Route path="/addNotice" element={<NewNotice />} />
-          <Route path="/editNotice/:id" element={<EditNotice />} />
+          <Route path={routes.mainPath()} element={<NoticeList />}/>
+          <Route path={routes.addNoticePath()} element={<NewNotice />} />
+          <Route path={routes.editNoticePaht()} element={<EditNotice />} />
         </Routes>
       </div>
     </BrowserRouter>
